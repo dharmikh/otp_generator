b6a8d2f36cdc8c43139a8a007c9b6878e06b20f0
dharm <dharmikbhaliya2542@gmail.com>
2023-02-19 09:08:17 +0530

    Otp Generator

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'otp.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: OTP(),
+    ),
+  );
+}
